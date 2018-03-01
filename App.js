b6087a7c9ac6cae46788e2d6b9e4064d9d46c76e
Sangwon Hong <qpakzk@gmail.com>
2018-03-01 19:14:51 +0900

    Save toDos
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- App.js
+++ App.js
@@ -9,19 +9,30 @@ import {
   Platform 
 } from 'react-native'
 
+import { AppLoading } from "expo";
 import ToDo from './ToDo'
-
+import uuidv1 from "uuid/v1"
 import { ScrollView } from 'react-native-gesture-handler';
 
 const { height, width } = Dimensions.get("window")
 
+
 export default class App extends Component {
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToDos: false,
+    toDos: {}
   }
 
+  componentDidMount() {
+    this._loadToDos()
+  }
   render() {
-    const { newToDo } = this.state
+    const { newToDo, loadedToDos, toDos } = this.state
+
+    if(!loadedToDos) {
+      return <AppLoading />
+    }
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Kawai To Do</Text>
@@ -34,9 +45,10 @@ export default class App extends Component {
           placeholderTextColor={"#999"}
           reternKeyType={"done"}
           autoCorrect={false}
+          onSubmitEditing={this._addToDo}
         />
         <ScrollView contentContainerStyle={styles.toDos}>
-          <ToDo />
+          {Object.values(toDos).map(toDo => <ToDo key={toDo.id} {... toDo}/>)}
         </ScrollView>
         </View>
       </View>
@@ -47,6 +59,40 @@ export default class App extends Component {
     this.setState({
       newToDo: text
     })
+  }
+
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    })
+  }
+
+  _addToDo = () => {
+    const { newToDo } = this.state
+    if (newToDo != "") {
+      this.setState(prevState => {
+        const ID = uuidv1()
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        }
+        
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos: {
+            ...prevState.toDOs,
+            ...newToDoObject
+          }
+        }
+
+        return {...newState}
+      })
+    }
   }
 }
 
--- ToDo.js
+++ ToDo.js
@@ -4,7 +4,8 @@ import {
     Text, 
     TouchableOpacity, 
     StyleSheet, 
-    Dimensions
+    Dimensions,
+    TextInput
 } from 'react-native'
 
 const { width, height } = Dimensions.get("window")
@@ -12,10 +13,12 @@ const { width, height } = Dimensions.get("window")
 export default class ToDo extends Component {
     state = {
         isEditing: false,
-        isCompleted: false
+        isCompleted: false,
+        toDoValue: ""
     }
     render() {
-        const { isCompleted, isEditing } = this.state
+        const { isCompleted, isEditing, toDoValue } = this.state
+        const { text } = this.props
         return(
             <View style={styles.container}>
                 <View style={styles.column}>
@@ -26,9 +29,23 @@ export default class ToDo extends Component {
                             ]} 
                         />
                     </TouchableOpacity>
-                    <Text style={
-                        [styles.text, 
-                        isCompleted ? styles.completedText : styles.uncompletedText]}>Hello I'am ToDo</Text>
+                    {isEditing ? (
+                        <TextInput 
+                            style={[
+                                styles.text,
+                                styles.input,
+                                isCompleted ? styles.completedText : styles.uncompletedText]} 
+                            value={toDoValue}
+                            multiline={true}
+                            onChangeText={this._controlInput}
+                            returnKeyType={"done"}
+                            onBlur={this._finishEditing}
+                        /> ): (
+                        <Text style={
+                            [styles.text,
+                            isCompleted ? styles.completedText : styles.uncompletedText]
+                        }>{ text }</Text>
+                    )}
                 </View>
                 {isEditing ? (
                     <View style={styles.actions}>
@@ -65,14 +82,22 @@ export default class ToDo extends Component {
     }
 
     _startEditing = () => {
+        const { text } = this.props
+
         this.setState({
-            isEditing: true
+            isEditing: true,
+            toDoValue: text
         })
     }
 
     _finishEditing = () => {
         this.setState({
             isEditing: false
+        })
+    }
+    _controlInput = text => {
+        this.setState({
+            toDoValue: text
         })
     }
 }
@@ -123,5 +148,9 @@ const styles = StyleSheet.create({
     actionContainer: {
         marginVertical: 10,
         marginHorizontal: 10
+    },
+    input: {
+        marginVertical: 15,
+        width: width / 2
     }
 })
